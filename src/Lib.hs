7ffcac4580cefcfadc99ecd05cd8069be4522c42
Стив <kenethra.74@gmail.com>
2022-06-17 20:11:57 -0600

    fix: <|> error

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,8 +4,13 @@ module Lib
 where
 
 import qualified Data.Char
+import Data.Foldable (Foldable (length), concatMap)
+import Data.Function
+import Data.List (genericIndex)
 import qualified Data.List
 import qualified Data.List.Split
+import Data.Maybe
+import Data.Traversable (Traversable (traverse))
 
 data Cell = Fixed Int | Possible [Int] deriving (Show, Eq)
 
@@ -37,6 +42,94 @@ showGridWithPossibilities = unlines . map (unwords . map showCell)
       (++ "]")
         . Data.List.foldl' (\acc x -> acc ++ if x `elem` xs then show x else " ") "["
         $ [1 .. 9]
+
+pruneCells :: [Cell] -> Maybe [Cell]
+pruneCells cells = traverse pruneCells cells
+  where
+    fixeds = [x | Fixed x <- cells]
+
+    pruneCells (Possible xs) = case xs Data.List.\\ fixeds of
+      [] -> Nothing
+      [y] -> Just $ Fixed y
+      ys -> Just $ Possible ys
+    pruneCells x = Just x
+
+subGridsToRow :: Grid -> Grid
+subGridsToRow =
+  concatMap
+    ( \rows ->
+        let [r1, r2, r3] = map (Data.List.Split.chunksOf 3) rows
+         in zipWith3 (\a b c -> a ++ b ++ c) r1 r2 r3
+    )
+    . Data.List.Split.chunksOf 3
+
+pruneGrid' :: Grid -> Maybe Grid
+pruneGrid' grid =
+  traverse pruneCells grid
+    >>= fmap Data.List.transpose . traverse pruneCells . Data.List.transpose
+    >>= fmap subGridsToRow . traverse pruneCells . subGridsToRow
+
+pruneGrid :: Grid -> Maybe Grid
+pruneGrid = fixM pruneGrid'
+  where
+    fixM f x = f x >>= \x' -> if x' == x then return x else fixM f x'
+
+nextGrids :: Grid -> (Grid, Grid)
+nextGrids grid =
+  let (i, first@(Fixed _), rest) =
+        fixCell
+          . Data.List.minimumBy (compare `Data.Function.on` (possibilityCount . snd))
+          . filter (isPossible . snd)
+          . zip [0 ..]
+          . concat
+          $ grid
+   in (replace2D i first grid, replace2D i rest grid)
+  where
+    isPossible (Possible _) = True
+    isPossible _ = False
+
+    possibilityCount (Possible xs) = length xs
+    possibilityCount (Fixed _) = 1
+
+    fixCell (i, Possible [x, y]) = (i, Fixed x, Fixed y)
+    fixCell (i, Possible (x : xs)) = (i, Fixed x, Possible xs)
+    fixCell _ = error "Impossible case"
+
+    replace2D :: Int -> a -> [[a]] -> [[a]]
+    replace2D i v =
+      let (x, y) = (i `quot` 9, i `mod` 9) in replace x (replace y (const v))
+    replace p f xs = [if i == p then f x else x | (x, i) <- zip xs [0 ..]]
+
+isGridFilled :: Grid -> Bool
+isGridFilled grid = null [() | Possible _ <- concat grid]
+
+isGridInvalid :: Grid -> Bool
+isGridInvalid grid =
+  any isInvalidRow grid
+    || any isInvalidRow (Data.List.transpose grid)
+    || any isInvalidRow (subGridsToRow grid)
+  where
+    isInvalidRow row =
+      let fixeds = [x | Fixed x <- row]
+          emptyPossibles = [x | Possible x <- row, null x]
+       in hasDups fixeds || not (null emptyPossibles)
+
+    hasDups l = hasDups' l []
+
+    hasDups' [] _ = False
+    hasDups' (y : ys) xs
+      | y `elem` xs = True
+      | otherwise = hasDups' ys (y : xs)
+
+solve :: Grid -> Maybe Grid
+solve grid = pruneGrid grid >>= solve'
+  where
+    solve' g
+      | isGridInvalid g = Nothing
+      | isGridFilled g = Just g
+      | otherwise =
+        let (grid1, grid2) = nextGrids g
+         in solve grid1 <|> solve grid2
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
